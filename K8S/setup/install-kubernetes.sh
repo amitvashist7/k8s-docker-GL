5080687c14953e4292cbe4d9c126b855c6749105
Amit Vashist <amitvashist7@outlook.com>
2019-04-13 16:23:44 +0530

    Update

--- K8S/setup/install-kubernetes.sh
+++ K8S/setup/install-kubernetes.sh
@@ -21,7 +21,7 @@ cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y kubelet=1.10.4-00 kubeadm=1.10.4-00 kubectl=1.10.4-00
+apt-get install -y kubelet kubeadm kubectl
 
 echo "deploying kubernetes (with calico)..."
 kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="172.31.0.100" 
